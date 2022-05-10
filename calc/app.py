5f12f8d4554dbefddb511f4ffbe328b47e125274
joshdarling <joshua.a.darling@gmail.com>
2022-05-09 23:37:03 -0600

    app.py in calc file now returns a single route/view function that can deal with 4 different kinds of URLs

--- calc/app.py
+++ calc/app.py
@@ -1,6 +1,6 @@
 # Put your app in here.
 from flask import Flask, request
-from operations import add
+from operations import *
 
 
 app = Flask(__name__)
@@ -13,5 +13,45 @@ def add_numbers():
     
     return str(add(a,b))
 
+@app.route('/sub')
+def sub_numbers():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    return str(sub(a,b))
+
+@app.route('/mult')
+def mult_numbers():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    return str(mult(a,b))
+
+@app.route('/div')
+def divide_nums():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+
+    return str(div(a,b))
 
 
+
+
+
+
+@app.route('/math/<id>')
+def find_operation(id):
+    
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    
+    number_operations={
+    'add': add(a,b),
+    'sub': sub(a,b),
+    'mult': mult(a,b),
+    'div': div(a,b)
+}
+  
+    math = number_operations[id]
+    
+    return str(math) 
